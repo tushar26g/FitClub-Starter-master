283a8f7d75f7c587ca3d8bca9250967698952a19
tushar26g <tushar26g@gmail.com>
2025-10-20 13:19:43 +0530

    Added mobile view for dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,9 +4,11 @@ import './DashBoard.css';
 import Sidebar from './SideBar/Sidebar';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { useTheme } from "../../context/ThemeContext";
 
 const Dashboard = () => {
+   // THEME STATE
+  const { theme, toggleTheme } = useTheme();
   return (
     <div className="dashboard">
       <div className="dashboard-container">
@@ -14,16 +16,16 @@ const Dashboard = () => {
         <div className="dashboard-content">
           <Outlet /> {/* Dynamically renders child pages like MainDash, AddMember, MemberDetailsPage */}
           <ToastContainer
-  position="top-center"   // ⬅️ Position it in the center
-  autoClose={3000}
-  hideProgressBar={false}
-  newestOnTop={false}
-  closeOnClick
-  rtl={false}
-  pauseOnFocusLoss
-  draggable
-  pauseOnHover
-/>
+            position="top-center"   // ⬅️ Position it in the center
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
         </div>
       </div>
     </div>
